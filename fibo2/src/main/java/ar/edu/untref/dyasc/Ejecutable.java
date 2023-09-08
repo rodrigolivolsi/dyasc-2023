ddfd7f830a54dc02eb60764a85b8e17d8be21e0d
rodrigolivolsi <livolsi.rod@gmail.com>
2023-09-08 15:47:14 -0300

    Agregada clase de Validacion

--- fibo2/src/main/java/ar/edu/untref/dyasc/Ejecutable.java
+++ fibo2/src/main/java/ar/edu/untref/dyasc/Ejecutable.java
@@ -1,41 +1,32 @@
 package ar.edu.untref.dyasc;
 
 public class Ejecutable {
+
     public static void main(String[] args) {
-        // Verifico la cantidad de argumentos
-        if (args.length < 1) {
-            System.out.println("Debe ingresar al menos un argumento");
-            return;
-        } else if (args.length > 2) {
-            System.out.println("Demasiados argumentos");
-            return;
-        }
+        try {
+            Validacion.validarArgumentos(args);
 
-        int n = 0;
-        String opciones = "";
+            String opciones = "";
+            int n;
 
-        // Si esta presente -o, procesa opciones y argumentos
-        if (args[0].startsWith("-o=")) {
-            if (args.length < 2) {
-                System.out.println("Debe ingresar al menos un argumento");
-                return;
+            if (args[0].startsWith("-o=")) {
+                opciones = args[0].substring(3);
+                n = Integer.parseInt(args[1]);
+            } else {
+                opciones = "hd"; // Opción por defecto
+                n = Integer.parseInt(args[0]);
             }
-            opciones = args[0].substring(3);
-            if (opciones.length() != 2) {
-                System.out.println("Opciones no validas.");
-                return;
-            }
-            n = Integer.parseInt(args[1]);
-        } else {
-            opciones = "hd"; // Opción por defecto
-            n = Integer.parseInt(args[0]);
+
+            Validacion.validarOpciones(opciones);
+
+            char orientacion = opciones.charAt(0);
+            char direccion = opciones.charAt(1);
+
+            String output = Formateador.formateadorFibonacci(n, orientacion, direccion);
+
+            System.out.println(output);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }
-
-        char orientacion = opciones.charAt(0);
-        char direccion = opciones.charAt(1);
-
-        String output = Formateador.formateadorFibonacci(n, orientacion, direccion);
-
-        System.out.println(output);
     }
 }
--- fibo2/src/main/java/ar/edu/untref/dyasc/SimuladorDeEjecucion.java
+++ fibo2/src/main/java/ar/edu/untref/dyasc/SimuladorDeEjecucion.java
@@ -4,18 +4,18 @@ import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
 
-public class SimuladorDeEjecucion {
+public class SimuladorDeEjecucion {  //para simular la ejecucion del programa y capturar la salida, lo implemente para los tests y verificar la salida generada por el programa
     
     public static String capturarOutput(String[] args) {
-        OutputStream originalOut = System.out;
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outputStream));
+        OutputStream originalOut = System.out;  //Creo una instancia de OutputStream llamada originalOut y la inicializo con la salida estándar actual, que es la que se usa para mostrar mensajes en la consola.
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(); //Creo una instancia de ByteArrayOutputStream llamada outputStream. Esta clase es un flujo de salida que se utiliza para almacenar datos en memoria.
+        System.setOut(new PrintStream(outputStream)); //Aquí se cambia la salida estándar actual (que originalmente apuntaba a la consola) para que apunte a outputStream. 
+                                                      //Esto significa que todo lo que normalmente se imprimiría en la consola ahora se redirigirá y se almacenará en outputStream.
+        Ejecutable.main(args); // Llama al programa principal pasandole el array de cadenas 'args' como argumento
 
-        Ejecutable.main(args); // Llama al programa principal
+        System.setOut(new PrintStream(originalOut)); //Luego de ejecutar el programa principal, esta línea restaura la salida estándar original, que apunta nuevamente a la consola.
 
-        System.setOut(new PrintStream(originalOut));
-
-        return outputStream.toString().trim();
+        return outputStream.toString().trim(); // El contenido capturado en outputStream se convierte en una cadena llamada output y se elimina cualquier espacio en blanco adicional. Esta cadena es lo que devuelve la función capturarOutput.
     }
 
 }
